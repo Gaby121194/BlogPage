9af08082102898a7bae087029fc363addafca1d8
Luca Beltramini <>
2021-12-10 20:48:53 +0000

    Added Comment.cs

---
+++ ITsynch.Trainings.LBGC.Demo/ITsynch.Trainings.LBGC.Demo.Server/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ITsynch.Trainings.LBGC.Demo.Models
+{
+    public class Comment : BaseEntity
+    {
+        public virtual string Username { get; set; }
+
+        public virtual string Content { get; set; }
+
+        public virtual DateTime Date { get; set; }
+    }
+}
