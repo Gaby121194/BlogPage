b2848f4775815895822d36d64994296667fbd46c
Luca Beltramini <>
2021-12-10 20:49:05 +0000

    Added CommentsService.cs

---
+++ ITsynch.Trainings.LBGC.Demo/ITsynch.Trainings.LBGC.Demo.Server/Services/CommentsService.cs
@@ -0,0 +1,28 @@
+﻿using ITsynch.Trainings.LBGC.Demo.Models;
+using ITsynch.Trainings.LBGC.Demo.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITsynch.Trainings.LBGC.Demo.Services
+{
+    public class CommentsService
+    {
+        private readonly TrainingsDemoContext trainingsDemoContext;
+
+        public CommentsService(
+            TrainingsDemoContext trainingsDemoContext)
+        {
+            this.trainingsDemoContext = trainingsDemoContext
+                ?? throw new ArgumentNullException(nameof(trainingsDemoContext));
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllCommentAsync()
+        {
+            var comments = await this.trainingsDemoContext.Comments.ToListAsync();
+            return comments.AsEnumerable();
+        }
+    }
+}
