d1599287cf776ee281af399333944cbff5a67b4c
Luca Beltramini <>
2021-12-10 20:48:18 +0000

    Added 2 files to DataTransfer

---
+++ ITsynch.Trainings.LBGC.Demo/ITsynch.Trainings.LBGC.Demo.Server/DataTransfer/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ITsynch.Trainings.LBGC.Demo.DataTransfer
+{
+    public class CommentDto
+    {
+        public virtual long Id { get; set; }
+
+        public virtual string Username { get; set; }
+
+        public virtual string Content { get; set; }
+
+        public virtual DateTime Date { get; set; }
+    }
+}
---
+++ ITsynch.Trainings.LBGC.Demo/ITsynch.Trainings.LBGC.Demo.Server/DataTransfer/CommentProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ITsynch.Trainings.LBGC.Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITsynch.Trainings.LBGC.Demo.DataTransfer
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            CreateMap<Comment, CommentDto>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Username, opts => opts.MapFrom(src => src.Username))
+                .ForMember(dest => dest.Content, opts => opts.MapFrom(src => src.Content))
+                .ForMember(dest => dest.Date, opts => opts.MapFrom(src => src.Date));
+        }
+    }
+}
